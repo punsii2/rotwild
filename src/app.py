1d0938b55daf3fff00378945fb44af81ce119dfc
Paul Menhart <paul@menhart.net>
2023-03-23 23:46:25 +0100

    add st.map plot for every id
    .

--- src/app.py
+++ src/app.py
@@ -1,22 +1,66 @@
 from urllib.error import URLError
+import matplotlib
 import numpy as np
 import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
+
 @st.cache_data
 def read_data() -> pd.DataFrame:
-    #XXX cleanup data
-    #XXX rename columns
-    return pd.read_csv("../red_deer_berchtesgarden_national_park.csv")
+    df = pd.read_csv("../red_deer_berchtesgarden_national_park.csv")
+    df = df.rename(columns={"location-lat": "lat", "location-long": "lon"})
+    # XXX remove unused columns, but maybe use the 'manually-marked-outlier' column previously
+    # Data columns (total 22 columns):
+    # #   Column                           Non-Null Count   Dtype
+    # ---  ------                           --------------   -----
+    # 0   event-id                         253273 non-null  int64
+    # 1   visible                          253273 non-null  bool
+    # 2   timestamp                        253273 non-null  object
+    # 3   location-long                    252769 non-null  float64
+    # 4   location-lat                     252769 non-null  float64
+    # 5   algorithm-marked-outlier         0 non-null       float64
+    # 6   comments                         223011 non-null  object
+    # 7   cpu-temperature                  30262 non-null   float64
+    # 8   gps:dop                          253273 non-null  float64
+    # 9   gps:fix-type-raw                 253273 non-null  object
+    # 10  gps:satellite-count              30262 non-null   float64
+    # 11  gps:twilight                     30262 non-null   object
+    # 12  height-above-ellipsoid           240159 non-null  float64
+    # 13  height-raw                       12607 non-null   float64
+    # 14  manually-marked-outlier          0 non-null       float64
+    # 15  mortality-status                 30262 non-null   object
+    # 16  transmission-protocol            30262 non-null   object
+    # 17  sensor-type                      253273 non-null  object
+    # 18  individual-taxon-canonical-name  253273 non-null  object
+    # 19  tag-local-identifier             253273 non-null  int64
+    # 20  individual-local-identifier      253273 non-null  int64
+    # 21  study-name                       253273 non-null  object
+    # dtypes: bool(1), float64(9), int64(3), object(9)
+
+    # diff = df['tag-local-identifier'] - df['individual-local-identifier']
+    # df = df[diff!=0] ==> none are left => these columns are identical
+    # XXX cleanup data
+    df = df.dropna(subset=["lat", "lon"])
+    df = df[df["algorithm-marked-outlier"] != None]
+    df = df[df["manually-marked-outlier"] != None]
+    df = df.drop(columns=["manually-marked-outlier", "algorithm-marked-outlier"])
+
+    df = df.drop(columns=["comments"])
+    # XXX remove this line
+    #df = df.dropna()
+    df.info()
+    return df
+
 
 @st.cache_data
 def from_data_file(filename: str):
     url = (
         "http://raw.githubusercontent.com/streamlit/"
         "example-data/master/hello/v1/%s" % filename
-        )
+    )
     return pd.read_json(url)
+
 
 def example_app():
     try:
@@ -91,19 +135,25 @@ def example_app():
             % e.reason
         )
 
+
 def main():
-    #example_app()
+    # example_app()
     df = read_data()
+    st.write("shape:")
+    st.write(df.shape)
     st.write(df)
-    st.write(df.info())
-    st.write(df.shape)
-    diff = df['tag-local-identifier'] - df['individual-local-identifier']
-    df = df[diff!=0]
-    st.write(df.shape)
 
-    #columns=['lat', 'lon'])
-    st.map(df)
 
+
+    groups = df.groupby("tag-local-identifier")
+    st.write(groups)
+    st.write(groups.groups)
+    for name, group in groups:
+        st.write(name)
+        st.write(group)
+        st.map(group)
+
+    #st.write(pd.plotting.scatter_matrix(df._get_numeric_data()))
 
 
 if __name__ == "__main__":
